29861a40c1f545ea5d11eb55c5c8fabd58060380
Spongebob <113505492+dobopalma@users.noreply.github.com>
2023-03-22 10:00:20 +0100

    Delete do1.py

--- do1.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Befogó: "))
-c = int(input("Befogó: "))
-b = (a * a + c * c) ** (1 / 2)
-k = a + b + c
-t = a * b / 2
-print(f"A háromszög kerülete: {k}")
-print(f"A háromszög területe: {t}")
